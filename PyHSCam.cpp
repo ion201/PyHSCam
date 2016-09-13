d929d83dc2aa3ac1e853acbe0c67d9442c529146
nate <moniseat101@yahoo.com>
2016-09-13 15:38:13 -0700

    update

--- PyHSCam.cpp
+++ PyHSCam.cpp
@@ -19,7 +19,8 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-// Disable security warnings because accoring to microsoft, snprintf "may be unsafe"...
+// Disable security warnings because accoring to microsoft,
+// sprintf and family "may be unsafe"...
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
@@ -32,11 +33,9 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <exception>
 #include <string>
 #include <sstream>
+#include <ctime>
 
 #include "PDCLIB.h"
-
-// Max buffer length for exception messages
-#define MSG_BUF_MAX_LEN 80
 
 
 // Function prototypes
@@ -45,6 +44,12 @@ void
 
 uint64_t
     PyHSCam_openDeviceByIp(const char * ipStr);
+
+void
+    PyHSCam_setDeviceStatus(uint64_t interfaceId, unsigned long newStatus);
+
+unsigned long
+    PyHSCam_getCurrentCapRate(uint64_t interfaceId);
 
 boost::python::list
     PyHSCam_getValidCapRates(uint64_t interfaceId);
@@ -58,8 +63,11 @@ boost::python::tuple
 void
     PyHSCam_setResolution(uint64_t interfaceId, unsigned long width, unsigned long height);
 
-PyObject *  // Py_Bytes
-    PyHSCam_captureImage(uint64_t interfaceId);
+PyObject *  // PyBytes
+    PyHSCam_captureLiveImage(uint64_t interfaceId);
+
+PyObject * // PyBytes
+    PyHSCam_getImageFromMemory(uint64_t interfaceId, unsigned long frameN);
 
 boost::python::list
     PyHSCam_getAllValidResolutions(uint64_t interfaceId);
@@ -68,7 +76,19 @@ bool
     PyHSCam_isDeviceMonochromatic(uint64_t interfaceId);
 
 void
-    PyHSCam_recordFrames(uint64_t interfaceId, uint32_t nFrames);
+    PyHSCam_beginRecording(uint64_t interfaceId);
+
+void
+    PyHSCam_haltRecording(uint64_t interfaceId);
+
+void
+    PyHSCam_recordBlocking(uint64_t interfaceId, uint64_t duration);
+
+unsigned long
+    PyHSCam_getStatus(uint64_t interfaceId);
+
+long
+    PyHSCam_getMemoryFrameCount(uint64_t interfaceId);
 
 
 // Combine deviceNum and childNum into a single uint64_t
@@ -122,6 +142,7 @@ public:
 PyObject * createExceptionClass(const char * name, PyObject * baseTypeObj = PyExc_Exception)
 {
     // Create an exception that will be exposed in python as ModuleName.name
+    // Thanks "user763305" on StackOverflow!
     std::string scopeName = boost::python::extract<std::string>(boost::python::scope().attr("__name__"));
     std::string qualifiedName0 = scopeName + "." + name;
     char * qualifiedName1 = const_cast<char *>(qualifiedName0.c_str());
@@ -141,9 +162,10 @@ void convertCppExceptionToPy(CamRuntimeError const &except)
     // by createExceptionClass() and assigns a formatted string for it's message.
     if (pyCamRuntimeError == NULL)
     {
-        throw std::runtime_error("Module attempted to raise exception before it was initialized."
+        throw std::runtime_error("Module somehow managed to raise an exception before the module was initialized\n"
                                  "This should never happen.");
     }
+#define MSG_BUF_MAX_LEN 80
     char msgBuf[MSG_BUF_MAX_LEN];
     if (except.hasErrorCode())
     {
@@ -153,9 +175,11 @@ void convertCppExceptionToPy(CamRuntimeError const &except)
                     "Error Code: %d",
                     except.getMessage().c_str(),
                     except.getErrorCode());
+        // Add the ".errorCode" attribute so that this exception can be caught and optionally
+        // checked for specific error codes.
         PyObject_SetAttrString(pyCamRuntimeError,
-                        "errorCode",
-                        PyLong_FromUnsignedLong(except.getErrorCode()));
+                                "errorCode",
+                                PyLong_FromUnsignedLong(except.getErrorCode()));
     }
     else
     {
@@ -227,9 +251,9 @@ uint64_t PyHSCam_openDeviceByIp(const char * ipStr)
     {
         char msgBuf[40];
         snprintf(&msgBuf[0],
-            40,
-            "SDK found unexpected ip = %#08x\n",
-            detectedNumInfo.m_DetectInfo[0].m_nTmpDeviceNo);
+                    40,
+                    "SDK found unexpected ip = %#08x\n",
+                    detectedNumInfo.m_DetectInfo[0].m_nTmpDeviceNo);
         throw CamRuntimeError(msgBuf);
     }
     //TODO: Should we specify the model to validate the camera?
@@ -257,12 +281,43 @@ uint64_t PyHSCam_openDeviceByIp(const char * ipStr)
     return IFACE_ID_FROM_VALUES(deviceNum, childNum);
 }
 
+unsigned long PyHSCam_getCurrentCapRate(uint64_t interfaceId)
+{
+    unsigned long retVal;
+    unsigned long errorCode;
+    unsigned long capRate;
+
+    retVal = PDC_GetRecordRate(IFACE_ID_GET_DEV_NUM(interfaceId),
+                                IFACE_ID_GET_CHILD_NUM(interfaceId),
+                                &capRate,
+                                &errorCode);
+    if (retVal == PDC_FAILED)
+    {
+        throw CamRuntimeError("Failed to retrieve current capture rate!", errorCode);
+    }
+    return capRate;
+}
+
+void PyHSCam_setDeviceStatus(uint64_t interfaceId, unsigned long newStatus)
+{
+    unsigned long retVal;
+    unsigned long errorCode;
+
+    retVal = PDC_SetStatus(IFACE_ID_GET_DEV_NUM(interfaceId),
+                            newStatus,
+                            &errorCode);
+    if (retVal == PDC_FAILED)
+    {
+        throw CamRuntimeError("Failed to set status!", errorCode);
+    }
+}
+
 boost::python::list PyHSCam_getValidCapRates(uint64_t interfaceId)
 {
     unsigned long retVal;
+    unsigned long errorCode;
     unsigned long modeCnt;
     unsigned long modeList[PDC_MAX_LIST_NUMBER];
-    unsigned long errorCode;
 
     retVal = PDC_GetRecordRateList(IFACE_ID_GET_DEV_NUM(interfaceId),
                                     IFACE_ID_GET_CHILD_NUM(interfaceId),
@@ -318,7 +373,7 @@ void PyHSCam_setCapRate(uint64_t interfaceId, unsigned long capRate)
     }
 }
 
-PyObject * PyHSCam_captureImage(uint64_t interfaceId)
+PyObject * PyHSCam_captureLiveImage(uint64_t interfaceId)
 {
     char * imageBuf;
     unsigned long imgWidth;
@@ -349,8 +404,115 @@ PyObject * PyHSCam_captureImage(uint64_t interfaceId)
                                     imageBuf,
                                     &errorCode);
 
+    if (retVal == PDC_FAILED)
+    {
+        free(imageBuf);
+        throw CamRuntimeError("Failed to retrive live image!", errorCode);
+    }
+
     PyObject * pyImageBuf = PyBytes_FromStringAndSize(imageBuf, imgBufSize);
     free(imageBuf);
+
+    return pyImageBuf;
+}
+
+
+PDC_FRAME_INFO PyHSCam_getMemoryFrameInfo(uint64_t interfaceId)
+{
+    unsigned long retVal;
+    unsigned long errorCode;
+    PDC_FRAME_INFO frameInfo;
+
+    if (PyHSCam_getStatus(interfaceId) != PDC_STATUS_PLAYBACK)
+    {
+        throw CamRuntimeError("Module attempted to read frame info without the status set to PLAYBACK.");
+    }
+
+    retVal = PDC_GetMemFrameInfo(IFACE_ID_GET_DEV_NUM(interfaceId),
+                                    IFACE_ID_GET_CHILD_NUM(interfaceId),
+                                    &frameInfo,
+                                    &errorCode);
+    if (retVal == PDC_FAILED)
+    {
+        throw CamRuntimeError("Failed to retrieve frame info!", errorCode);
+    }
+
+    return frameInfo;
+}
+
+
+long PyHSCam_getMemoryFrameCount(uint64_t interfaceId)
+{
+    if (PyHSCam_getStatus(interfaceId) != PDC_STATUS_PLAYBACK)
+    {
+        PyHSCam_setDeviceStatus(interfaceId, PDC_STATUS_PLAYBACK);
+    }
+
+    PDC_FRAME_INFO frameInfo;
+    frameInfo = PyHSCam_getMemoryFrameInfo(interfaceId);
+
+    PyHSCam_setDeviceStatus(interfaceId, PDC_STATUS_LIVE);
+
+    return frameInfo.m_nRecordedFrames;
+}
+
+
+PyObject * PyHSCam_getImageFromMemory(uint64_t interfaceId, unsigned long frameN)
+{
+    unsigned long retVal;
+    unsigned long errorCode;
+
+    // The mode must be set to PDC_STATUS_PLAYBACK to read from memory
+    if (PyHSCam_getStatus(interfaceId) != PDC_STATUS_PLAYBACK)
+    {
+        PyHSCam_setDeviceStatus(interfaceId, PDC_STATUS_PLAYBACK);
+    }
+
+    PDC_FRAME_INFO frameInfo;
+    frameInfo = PyHSCam_getMemoryFrameInfo(interfaceId);
+
+    if (frameN > frameInfo.m_nRecordedFrames)
+    {
+        throw CamRuntimeError("Failed to retrieve image - frameN is out of range for recorded images.");
+    }
+
+    char * imageBuf;
+    uint32_t imageBufSize;
+    unsigned long imgWidth;
+    unsigned long imgHeight;
+
+    boost::python::tuple imgResolution = PyHSCam_getCurrentResolution(interfaceId);
+    imgWidth = boost::python::extract<unsigned long>(imgResolution[0]);
+    imgHeight = boost::python::extract<unsigned long>(imgResolution[1]);
+
+    if (PyHSCam_isDeviceMonochromatic(interfaceId))
+    {
+        imageBufSize = imgWidth * imgHeight;
+    }
+    else  // *3 size to account for RGB
+    {
+        imageBufSize = imgWidth * imgHeight * 3;
+    }
+
+    imageBuf = (char *)malloc(imageBufSize);
+
+    retVal = PDC_GetMemImageData(IFACE_ID_GET_DEV_NUM(interfaceId),
+                                    IFACE_ID_GET_CHILD_NUM(interfaceId),
+                                    frameInfo.m_nTrigger + frameN,
+                                    8,                      // Assume 8-bit color depth
+                                    imageBuf,               // Output
+                                    &errorCode);            // Output
+
+    if (retVal == PDC_FAILED)
+    {
+        free(imageBuf);
+        throw CamRuntimeError("Failed to retrieve image from memory!", errorCode);
+    }
+
+    PyObject * pyImageBuf = PyBytes_FromStringAndSize(imageBuf, imageBufSize);
+    free(imageBuf);
+
+    PyHSCam_setDeviceStatus(interfaceId, PDC_STATUS_LIVE);
 
     return pyImageBuf;
 }
@@ -447,22 +609,40 @@ bool PyHSCam_isDeviceMonochromatic(uint64_t interfaceId)
     return false;
 }
 
-
-void PyHSCam_recordFrames(uint64_t interfaceId, uint32_t nFrames)
+unsigned long PyHSCam_getStatus(uint64_t interfaceId)
 {
     unsigned long retVal;
     unsigned long errorCode;
+    unsigned long deviceStatus;
+    retVal = PDC_GetStatus(IFACE_ID_GET_DEV_NUM(interfaceId),
+                            &deviceStatus,
+                            &errorCode);
+    if (retVal == PDC_FAILED)
+    {
+        throw CamRuntimeError("Error while retrieving device status!", errorCode);
+    }
+    return deviceStatus;
+}
 
-    // retVal = PDC_SetTriggerMode(IFACE_ID_GET_DEV_NUM(interfaceId),
-    //                             PDC_TRIGGER_MANUAL,
-    //                             nFrames,
-    //                             0,              // Unused
-    //                             0,              // Unused
-    //                             &errorCode);    // Output
-    // if (retVal == PDC_FAILED)
-    // {
-    //     throw CamRuntimeError("Failed to set trigger mode!", errorCode);
-    // }
+
+void PyHSCam_beginRecording(uint64_t interfaceId)
+{
+    // Begin recording endlessly (or until we run out of memory).
+    // This function does not handle exiting recording mode or halting recording.
+
+    unsigned long errorCode;
+    unsigned long retVal;
+
+    retVal = PDC_SetTriggerMode(IFACE_ID_GET_DEV_NUM(interfaceId),
+                                PDC_TRIGGER_CENTER,
+                                0,              // nAFrames: Unused in endless mode
+                                0,              // nRFrames: Unused in endless mode
+                                0,              // nRCount:  Unused in endless mode
+                                &errorCode);    // Output
+    if (retVal == PDC_FAILED)
+    {
+        throw CamRuntimeError("Failed to set trigger mode!", errorCode);
+    }
 
     retVal = PDC_SetRecReady(IFACE_ID_GET_DEV_NUM(interfaceId),
                                 &errorCode);     // Output
@@ -474,56 +654,79 @@ void PyHSCam_recordFrames(uint64_t interfaceId, uint32_t nFrames)
 
     // This should confirm the operating mode of the device
     unsigned long deviceStatus;
-    // TODO: Should probably have a more reliable timeout
-    uint32_t i;
-#define STATUS_CHECK_TIMEOUT 2000
-    for (i = 0; i < STATUS_CHECK_TIMEOUT; i++)
+    // TODO: Could switch to <chrono> library for sub-ms precision
+    clock_t startTime;
+    // Timeout in ms. This check confirms that the camera is operating in record mode
+    // before we begin recording.
+#define STATUS_CHECK_TIMEOUT 1000
+    startTime = clock();
+    bool actionCompleted = false;
+    while ((clock() - startTime) < STATUS_CHECK_TIMEOUT)
     {
-        retVal = PDC_GetStatus(IFACE_ID_GET_DEV_NUM(interfaceId),
-                                &deviceStatus,      // Output
-                                &errorCode);        // Output
-        if (retVal == PDC_FAILED)
-        {
-            throw CamRuntimeError("Device status check failed!", errorCode);
-        }
+        deviceStatus = PyHSCam_getStatus(interfaceId);
         if ((deviceStatus == PDC_STATUS_RECREADY) ||
             (deviceStatus == PDC_STATUS_REC))
         {
             // The device is ready for recording
+            actionCompleted = true;
             break;
         }
     }
-    if (i == STATUS_CHECK_TIMEOUT)
+    if (!actionCompleted)
     {
         throw CamRuntimeError("Function timed out while waiting for device to enter record-ready state.");
     }
 
     // Start the recording
-    retVal = PDC_TriggerIn(IFACE_ID_GET_DEV_NUM(interfaceId),
+    retVal = PDC_SetEndless(IFACE_ID_GET_DEV_NUM(interfaceId),
                             &errorCode);
     if (retVal == PDC_FAILED)
     {
         throw CamRuntimeError("Device trigger in (begin recording) failed!", errorCode);
     }
+}
 
+void PyHSCam_haltRecording(uint64_t interfaceId)
+{
+    // Assumes that the device is in recording mode and actively recording
+    // Halt it forcibly by setting the mode to "LIVE"
+    unsigned long retVal;
+    unsigned long errorCode;
+
+    PyHSCam_setDeviceStatus(interfaceId, PDC_STATUS_LIVE);
+}
+
+
+void PyHSCam_recordBlocking(uint64_t interfaceId, uint64_t duration)
+{
+    unsigned long deviceStatus;
+
+    clock_t startTime;
+    startTime = clock();
+
+    // Add 5 ms to account for the time between now and when the recording actually starts.
+    // (Not that precision is too important, but I think it looks nicer if the timer is accurate)
+    // Starting the timer after PyHSCam_beginRecording() is called introduces ~200 ms of extra
+    // uncounted recording time.
+    duration += 5;
+
+    PyHSCam_beginRecording(interfaceId);
+
+    clock_t curTime;
     // Wait for recording to finish
-    // TODO: Should have a timeout. We can guess duration with capture rate and nFrames.
-    for (i = 0; i < 20000; i++)
+    do
     {
-        retVal = PDC_GetStatus(IFACE_ID_GET_DEV_NUM(interfaceId),
-                                &deviceStatus,
-                                &errorCode);
-        if (retVal == PDC_FAILED)
-        {
-            throw CamRuntimeError("Error while retrieving device status!", errorCode);
-        }
-        if ((deviceStatus != PDC_STATUS_RECREADY) &&
+        deviceStatus = PyHSCam_getStatus(interfaceId);
+        if ((deviceStatus != PDC_STATUS_ENDLESS) &&
             (deviceStatus != PDC_STATUS_REC))
         {
             // Recording has finished.
             break;
         }
-    }
+        curTime = clock();
+    } while ((curTime - startTime) < duration);
+
+    PyHSCam_haltRecording(interfaceId);
 }
 
 
@@ -544,20 +747,24 @@ BOOST_PYTHON_MODULE(PyHSCam)
     boost::python::def("openDeviceByIp",
                         PyHSCam_openDeviceByIp,
                         boost::python::args("targetIp"),
-                        "Opens device at the specified ip address.\n"
+                        "Opens device at the specified ip address. "
                         "Returns interfaceId representing the opened device.");
     boost::python::def("setCapRate",
                         PyHSCam_setCapRate,
                         boost::python::args("interfaceId", "captureRate"),
                         "Attempts to set specified device to a particular capture rate.");
+    boost::python::def("getCurrentCapRate",
+                        PyHSCam_getCurrentCapRate,
+                        boost::python::args("interfaceId"),
+                        "Returns the current capture rate for interfaceId");
     boost::python::def("getValidCapRates",
                         PyHSCam_getValidCapRates,
                         boost::python::args("interfaceId"),
                         "Get a list of all valid capture rates for the specified device.");
     boost::python::def("captureLiveImage",
-                        PyHSCam_captureImage,
+                        PyHSCam_captureLiveImage,
                         boost::python::args("interfaceId"),
-                        "Captures an image and returns the data in a bytes object.\n"
+                        "Captures an image and returns the data in a bytes object. "
                         "By default, color images are in the interleave format (BGRBGR...)");
     boost::python::def("getCurrentResolution",
                         PyHSCam_getCurrentResolution,
@@ -570,10 +777,21 @@ BOOST_PYTHON_MODULE(PyHSCam)
     boost::python::def("getAllValidResolutions",
                         PyHSCam_getAllValidResolutions,
                         boost::python::args("interfaceId"),
-                        "Retrieves a list of valid resolutions for interfaceId.\n"
+                        "Retrieves a list of valid resolutions for interfaceId. "
                         "Each element is a tuple containing (width, height)");
-    boost::python::def("recordFrames",
-                        PyHSCam_recordFrames,
-                        boost::python::args("interfaceId", "nFrames"),
-                        "Capture nFrames frames on interfaceId and store images to internal memory.\n");
+    boost::python::def("recordBlocking",
+                        PyHSCam_recordBlocking,
+                        boost::python::args("interfaceId", "duration"),
+                        "Capture frames on interfaceId for the specified duration (in ms). "
+                        "This function blocks until either recording has completed or the device's "
+                        "internal memory fills up.");
+    boost::python::def("getImageFromMemory",
+                        PyHSCam_getImageFromMemory,
+                        boost::python::args("interfaceId", "frameN"),
+                        "Retrieve frame number 'frameN' taken by interfaceId which was previously "
+                        "saved to device memory. See also: getMemoryFrameCount().");
+    boost::python::def("getMemoryFrameCount",
+                        PyHSCam_getMemoryFrameCount,
+                        boost::python::args("interfaceId"),
+                        "Retrieve the number of frames in memory for the specified interfaceId.");
 }
